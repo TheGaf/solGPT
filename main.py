f12896fab857d718109a074224859e5988d2419f
The Gaf <bryangaffin@gmail.com>
2025-07-10 18:47:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = create_app()
 
 # 2) Prefetch your frontend HTML (sol.html) into app.page_html
 try:
-    ui_response = requests.get("https://gaf.nyc/sol.html", timeout=5)
+    ui_response = requests.get("sol.html", timeout=5)
     ui_response.raise_for_status()
     app.page_html = ui_response.text
 except Exception:
