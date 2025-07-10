83b00e9098a3d7b8919f00c8d8c3cf4e85591a2e
The Gaf <bryangaffin@gmail.com>
2025-07-10 18:50:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from app import create_app
 
-# 1) Build the Flask app
 app = create_app()
 
-# 2) If you run this file directly, launch Flask’s built-in server
 if __name__ == "__main__":
-    # match Render’s default port
     app.run(host="0.0.0.0", port=10000, debug=False)
