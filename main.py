82bf94c491b228d388ab00b292fe1514472de04a
The Gaf <bryangaffin@gmail.com>
2025-07-11 15:18:09 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import requests
+# main.py
 from app import create_app
 
 app = create_app()
-# Prefetch UI template
-try:
-    page_html = requests.get("https://gaf.nyc/sol.html", timeout=5).text
-except:
-    page_html = "<html><body>UI unavailable</body></html>"
-app.page_html = page_html
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=False)
+if __name__ == "__main__":
+    # when you run locally
+    app.run(host="0.0.0.0", port=10000, debug=False)
