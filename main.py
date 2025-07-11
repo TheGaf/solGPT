a55e273c1e6155771e9f79611c908da1dc4ae846
The Gaf <bryangaffin@gmail.com>
2025-07-10 23:20:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+# main.py
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    # in dev, use Flask’s built-in server on port 10000
+    # when you run locally
     app.run(host="0.0.0.0", port=10000, debug=False)
